e4432e5da379615098891a0d3233fcba94affbd0
Xyves <xodrixx@protonmail.com>
2023-10-01 20:46:53 +0200

    Add configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,43 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
+  watch: true,
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/images/[name][ext]",
+          publicPath: "./images",
+          outputPath: "./img",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+    ],
   },
 };

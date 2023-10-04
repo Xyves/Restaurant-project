5bf03083627ad8237038716930c1f537ce17711e
Xyves <xodrixx@protonmail.com>
2023-10-04 22:07:39 +0200

    Add Main JS and CSS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./input.css";
-import generateAside from "./sidebar.js";
+import navBar from "./nav-bar";
 import createSection from "./mainSection.js";
-import header from "./header.js";
+
 const body = document.querySelector("body");
 const container = document.querySelector(".container");
 
@@ -9,7 +9,6 @@ console.log("Does it work????");
 
 document.body.onload = loadDom();
 function loadDom() {
-  header();
-  generateAside();
+  navBar();
   createSection();
 }
